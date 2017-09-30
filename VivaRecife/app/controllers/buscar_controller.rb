aa3e2ea390607f17481b3faa70a64dce1e70013c
Carolina Melo <cmpm@cin.ufpe.br>
2017-09-30 19:19:40 -0300

    Rotas Principais

---
+++ VivaRecife/app/controllers/buscar_controller.rb
@@ -0,0 +1,2 @@
+class BuscarController < ApplicationController
+end
--- VivaRecife/config/routes.rb
+++ VivaRecife/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'home/index'
 
-  root 'welcome#index'
+  #resources :articles
+
+  root 'home#index'
 end
