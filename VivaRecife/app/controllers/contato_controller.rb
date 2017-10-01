902fff0004f096069d2fbdd4e90fb452fcf03de4
Carolina Melo <cmpm@cin.ufpe.br>
2017-10-01 17:31:14 -0300

    Rascunhos do index e rotas

--- VivaRecife/app/controllers/contato_controller.rb
+++ VivaRecife/app/controllers/contato_controller.rb
@@ -1,2 +1,9 @@
 class ContatoController < ApplicationController
+	def new
+		
+	end
+
+	def method_name
+		render plain: params[:contato].inspect
+	end
 end
--- VivaRecife/config/routes.rb
+++ VivaRecife/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  #resources :articles
+  resources :contato
 
   root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
