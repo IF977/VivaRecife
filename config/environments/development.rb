80c1c428f767a1a2ac7f6339006241d14871d79f
Jose Guilherme <jgnvs@cin.ufpe.br>
2017-11-27 13:18:21 -0200

    problema no envio de emails com heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_caching = false
+    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+    config.action_mailer.perform_caching = false
+    config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'vivanorecife.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :user_name => 'vivapelorecife@gmail.com',
+    :password => "frevowithtapioca",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 
   config.assets.compress = true
   config.assets.compile = true
